de584b56654a2d34f027d2a47b456fd5f162174a
Marlon E. Cobos <manubio13@gmail.com>
2018-09-12 00:31:25 -0500

    Variables correlation, lines for selection

--- Variables_processing/Variables_correlation_evaluation.R
+++ Variables_processing/Variables_correlation_evaluation.R
@@ -5,11 +5,11 @@
 # data needed 
 ## Environmental raster layers, in this example we will use a set of ascii 
 ## layers located in the folder (bio) in our working directory. Our layers are 
-## 10 bioclimatic varaibles. These varaibles can be in GeoTiff format as well.
+## 15 bioclimatic varaibles. These varaibles can be in other formats as well,
+## but make sure that you use the appropriate extension when reading them.
 
-## Note: All variables must have the same projection.
+## Note: All variables must have the same projection, extent, and resolution.
 ## We suggest to work with Geographic projections WGS84, with no planar projection.
-
 
 # loading needed packages (packages will be automatically installed if required)
 pcakages <- c("raster", "rgdal")
@@ -42,3 +42,32 @@ correlation_matrix <- cor(variables_values)
 # saving correlation matrix
 write.csv(correlation_matrix, "variables_correlation_matrix.csv",
           row.names = FALSE)
+
+# detecting correlated varaibles more easily
+correlation_matrix1 <- correlation_matrix # making other table with results
+
+max_cor <- 0.8 # maximum value of correlation allowed
+
+for (i in 1:dim(correlation_matrix1)[2]) { #correlated values will turn into 2 for easier detection
+  for (j in 1:dim(correlation_matrix1)[1]) {
+    correlation_matrix1[j, i] <- ifelse(correlation_matrix1[j, i] > max_cor | correlation_matrix1[j, i] < -max_cor, 
+                                        2, correlation_matrix1[j, i])
+  }
+}
+
+# #checking the table
+View(correlation_matrix1) # selection should be done manually, 2 = correlated
+
+# selecting variables and writing them in a new directory
+selected_variables <- variables[[c("bio_1", "bio_4", "bio_7", # select only non-correlated ones
+                                   "bio_12", "bio_15", "bio_17")]] 
+
+variable_names <- paste("Non_cor_variables/", names(selected_variables), # names of variables
+                        ".asc", sep = "") 
+
+dir.create("Non_correlated_variables") # create the folder for saving these variables
+
+for (i in 1:length(raster::unstack(selected_variables))) { # writing the selected variables
+  raster::writeRaster(selected_variables[[i]], filename = variable_names[i], 
+                      format = "ascii")
+}
