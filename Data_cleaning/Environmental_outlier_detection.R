860224ff20418453c9e08e135cb4cbceb2e1c315
Marlon E. Cobos <manubio13@gmail.com>
2018-09-13 22:03:24 -0500

    Env_outliers detection improved

--- Data_cleaning/Environmental_outlier_detection.R
+++ Data_cleaning/Environmental_outlier_detection.R
@@ -3,13 +3,13 @@
 #####
 
 # data needed 
-## (1) Species occurrences, in this example we will use a file named sp_occ.csv. 
+## (1) Species occurrences; in this example we will use a file named sp_occ.csv. 
 ## This file must contain the following columns (in that order): 
-## Species_name, Longitud, Latitud.
+## ID, Species_name, Longitud, Latitud.
 
-## (2) Environmental raster layers, in this example we will use two ascii 
+## (2) Environmental raster layers; in this example we will use two ascii 
 ## layers located in the folder (bio) in our working directory. Our layers are 
-## Mean annual temperature and Annual Precipitation These varaibles can be in 
+## Mean annual temperature and Annual Precipitation. These varaibles can be in 
 ## GeoTiff format as well.
 
 ## (3) The region of interest, accessioble area (M), or calibration area.
@@ -23,7 +23,7 @@
 
 
 # loading needed packages (packages will be automatically installed if required)
-pcakages <- c("raster", "rgdal", "rgeos")
+pcakages <- c("raster", "rgdal")
 req_packages <- pcakages[!(pcakages %in% installed.packages()[, "Package"])]
 if (length(req_packages) > 0) {
   install.packages(req_packages, dependencies = TRUE)
@@ -31,12 +31,12 @@ if (length(req_packages) > 0) {
 sapply(pcakages, require, character.only = TRUE)
 
 # defining working directory
-setwd("D:/Marlon/Data_cleaning/") # change this to your working directory
+setwd("C:/Users/Marlon/Documents/R/Data_cleaning") # change this to your working directory
 
 # reading data
-occurrences <- read.csv("sp_occ.csv") # occurrences
+occurrences <- read.csv("Cynomys_ludovicianus_Exercise.csv") # occurrences
 
-variables <- stack("bio/bio_1", "bio/bio_12") # stack of variables
+variables <- stack("bio/bio_1.asc", "bio/bio_12.asc") # stack of variables
 
 M <- readOGR(dsn = ".", layer = "acc_area")# accessible area (M), or calibration area
 
@@ -44,35 +44,45 @@ M <- readOGR(dsn = ".", layer = "acc_area")# accessible area (M), or calibration
 # M <- raster("acc_area.asc")
 
 # masking variables to the calibration area
-m_variables <- mask(varaibles, M)
+m_variables <- crop(variables, M)
+m_variables <- mask(m_variables, M)
 
 # getting data from the variables
 variables_values <- na.omit(values(m_variables)) # for the region of interest
 
-occ_variables <- na.omit(extract(m_variables, occ[, 2:3]))
+occ_variables <- na.omit(cbind(occurrences[, 1], # for occurrences adding IDs
+                               extract(m_variables, occurrences[, 3:4])))
 
 # sample of 10000 values if more pixels exist
-if (dim(variables_values > 10000)) {
-  variables_values <- variables_values[sample(rownames(variables_values), 10000), ] 
+if (dim(variables_values)[1] > 10000) {
+  variables_values <- variables_values[sample(1:dim(variables_values)[1], 10000), ] 
 } 
 
 # plot for searching for potential environmental outliers
-par(mar = c(4.5, 4, 0.5, 0.5), cex = 0.9)
-plot(variables_values[, 1], variables_values[, 2], col = "gray75",
+par(mfcol = c(1, 2), mar = c(4.5, 4, 0.5, 0.5), cex = 0.9)
+plot(variables_values[, 1], variables_values[, 2], col = "gray65",
      pch = 1, xlab = "Temperature", ylab = "Precipitation")
-points(occ_variables[, 1], occ_variables[, 2], col = "black",
+points(occ_variables[, 2], occ_variables[, 3], col = "black",
        pch = 19)
 legend("topleft", legend = c("Region of interest", "Occurrences"),
-       pch = c(1, 19), col = c("gray75", "black"))
+       pch = c(1, 19), col = c("gray65", "black"), bty = "n")
+plot(variables_values[, 1], variables_values[, 2], col = "gray65",
+     pch = 1, xlab = "Temperature", ylab = "")
+text(occ_variables[, 2], occ_variables[, 3], occ_variables[, 1], cex = 0.6)
+legend("topleft", legend = "Occurrence ID", bty = "n")
 
 # if need to save the figure use the following code
-jpeg(filename = "Environmental_outliers.jpg", width = 166, height = 166,
+jpeg(filename = "Environmental_outliers.jpg", width = 166, height = 83,
      units = "mm", res = 600)
-par(mar = c(4.5, 4, 0.5, 0.5), cex = 0.9)
-plot(variables_values[, 1], variables_values[, 2], col = "gray75",
+par(mfcol = c(1, 2), mar = c(4.5, 4, 0.5, 0.5), cex = 0.65)
+plot(variables_values[, 1], variables_values[, 2], col = "gray65",
      pch = 1, xlab = "Temperature", ylab = "Precipitation")
-points(occ_variables[, 1], occ_variables[, 2], col = "black",
+points(occ_variables[, 2], occ_variables[, 3], col = "black",
        pch = 19)
 legend("topleft", legend = c("Region of interest", "Occurrences"),
-       pch = c(1, 19), col = c("gray75", "black"))
+       pch = c(1, 19), col = c("gray65", "black"), bty = "n")
+plot(variables_values[, 1], variables_values[, 2], col = "gray65",
+     pch = 1, xlab = "Temperature", ylab = "")
+text(occ_variables[, 2], occ_variables[, 3], occ_variables[, 1], cex = 0.6)
+legend("topleft", legend = "Occurrence ID", bty = "n")
 dev.off()  
