205197721ba2e77279c337bb779ef40982c3dc56
Marlon E. Cobos <manubio13@gmail.com>
2018-11-09 14:00:39 -0600

    GBIF obstacles passed

--- Getting_data/get_GBIF_data.R
+++ Getting_data/get_GBIF_data.R
@@ -54,7 +54,9 @@ if (sum(counts) == 0) { # if no info, tell the species
     key <- keysco[dim(keysco)[1], 1] # detecting species key that return information
   }
   
-  occ <- occ_search(taxonKey = key, return = "data", limit = 10000) # getting the data from GBIF
+  occ <- occ_search(taxonKey = key, return = "data", limit = 10000) # getting the data from GBIF, 
+                                                                    # repeat if time-related error appears
+                                                                    # don't try again if species is not in GBIF
   occ_g <- occ
   occ_g <- occ_g[, c(1, 2, 4, 3, 5:dim(occ_g)[2])] # reordering longitude and latitude
   
@@ -84,52 +86,74 @@ spvector <- c("Dasypus kappleri", "Panthera onca", "Dasyprocta punctata") # bino
 occ_count <- list() # object to save info on number of georeferenced records per species 
 
 for (i in 1:length(spvector)) {
-  sps <- name_lookup(query = spvector[i], rank = "species", 
-                     return = "data", limit = 100) # information about the species
+  sps <- try(name_lookup(query = spvector[i], rank = "species", 
+                         return = "data", limit = 100), silent = TRUE) # information about the species
   
-  keys <- sps$key # all keys returned
-  counts <- vector() # object to save info on number of records per key
+  sps_class <- class(sps)
   
-  for (j in 1:length(keys)) { # testing if keys return records
-    counts[j] <- occ_count(taxonKey = keys[j], georeferenced = TRUE) 
-  }
-  
-  if (sum(counts) == 0) { # if no info, tell the species
-    cat("species", spvector[i], "has no goereferenced data\n")
+  # avoiding errors from GBIF (e.g., species name not in GBIF)
+  if(sps_class[1] == "try-error") {
+    occ_count[[i]] <- c(Species = spvector[i], keys = 0, counts = 0) # species not in GBIF
+    cat("species", spvector[i], "is not in the GBIF database\n")
     
-  }else { # if it has info, use the key with more records, which is the most useful
-    if (length(keys) == 1) { # if it is only one key
-      key <- keys # detecting species key 
-      occ_count[[i]] <- cbind(spvector[i], counts) # count how many records
-      
-    }else { # if its more than one key
-      keysco <- cbind(keys, counts)
-      keysco <- keysco[order(keysco[, 2]), ]
-      key <- keysco[dim(keysco)[1], 1] # detecting species key that return information
-      occ_count[[i]] <- keysco[dim(keysco)[1], ]# count how many records
+  }else {
+    keys <- sps$key # all keys returned
+    counts <- vector() # object to save info on number of records per key
+    
+    for (j in 1:length(keys)) { # testing if keys return records
+      counts[j] <- occ_count(taxonKey = keys[j], georeferenced = TRUE) 
     }
     
-    occ <- occ_search(taxonKey = key, return = "data", limit = 10000) # getting the data from GBIF
-    occ_g <- occ
-    occ_g <- occ_g[, c(1, 2, 4, 3, 5:dim(occ_g)[2])] # reordering longitude and latitude
-    
-    # keeping only unique georeferenced records. IF NO FILTERING IS NEEDED, PUT A # IN FRONT OF THE NEXT 3 LINES
-    occ_g <- occ_g[!is.na(occ_g$decimalLatitude) & !is.na(occ_g$decimalLongitude), ] # excluding no georeferences
-    occ_g <- occ_g[!duplicated(paste(occ_g$name, occ_g$decimalLatitude, # excluding duplicates
-                                     occ_g$decimalLongitude, sep = "_")), ]
-    
-    # writting file
-    file_name <- paste(gsub(" ", "_", spvector[i]), "csv", sep = ".") # csv file name per each species
-    write.csv(occ_g, file_name, row.names = FALSE) # writing inside each genus folder
-    
-    cat(i, "of", length(spvector), "species\n") # counting species per genus 
+    if (sum(counts) == 0) { # if no info, tell the species
+      occ_count[[i]] <- c(Species = spvector[i], keys = "all", counts = 0) # species not in GBIF
+      cat("species", spvector[i], "has no goereferenced data\n")
+      
+    }else { # if it has info, use the key with more records, which is the most useful
+      if (length(keys) == 1) { # if it is only one key
+        key <- keys # detecting species key 
+        occ_count[[i]] <- cbind(spvector[i], counts) # count how many records
+        
+      }else { # if its more than one key
+        keysco <- cbind(keys, counts)
+        keysco <- keysco[order(keysco[, 2]), ]
+        key <- keysco[dim(keysco)[1], 1] # detecting species key that return information
+        occ_count[[i]] <- c(Species = spvector[i], keysco[dim(keysco)[1], ])# count how many records
+      }
+      
+      occ <- try(occ_search(taxonKey = key, return = "data", limit = 10000), silent = TRUE) # getting the data from GBIF
+      occ_class <- class(occ)
+      
+      # avoiding errors from GBIF
+      while (occ_class[1] == "try-error") {
+        occ <- try(occ_search(taxonKey = key, return = "data", limit = 10000), silent = TRUE) # getting the data from GBIF
+        occ_class <- class(occ)
+        
+        if(occ_class[1] != "try-error") {
+          break()
+        }
+      }
+      
+      # following steps
+      occ_g <- occ
+      occ_g <- occ_g[, c(1, 2, 4, 3, 5:dim(occ_g)[2])] # reordering longitude and latitude
+      
+      # keeping only unique georeferenced records. IF NO FILTERING IS NEEDED, PUT A # IN FRONT OF THE NEXT 3 LINES
+      occ_g <- occ_g[!is.na(occ_g$decimalLatitude) & !is.na(occ_g$decimalLongitude), ] # excluding no georeferences
+      occ_g <- occ_g[!duplicated(paste(occ_g$name, occ_g$decimalLatitude, # excluding duplicates
+                                       occ_g$decimalLongitude, sep = "_")), ]
+      
+      # writting file
+      file_name <- paste(gsub(" ", "_", spvector[i]), "csv", sep = ".") # csv file name per each species
+      write.csv(occ_g, file_name, row.names = FALSE) # writing inside each genus folder
+      
+      cat(i, "of", length(spvector), "species\n") # counting species per genus 
+    }
   }
 }
 
-occ_count[sapply(occ_count, is.null)] <- NULL # excluding countings with no data
 genus_data <- do.call(rbind, occ_count) # making the list of countings a table
-genus_data <- data.frame(genus_data[, 1], as.numeric(genus_data[, 2])) # making countings numeric
-names(genus_data) <- c("Species", "N_records") # naming columns
+genus_data <- data.frame(genus_data[, 1], genus_data[, 2], as.numeric(genus_data[, 3])) # making countings numeric
+names(genus_data) <- c("Species", "Key", "N_records") # naming columns
 
 # writing the table
 file_nam <- "Species_record_count.csv" # csv file name for all species
@@ -172,52 +196,74 @@ for (h in 1:length(genus)) {
   occ_count <- list() # object to save info on number of georeferenced records per species 
   
   for (i in 1:length(spvector)) {
-    sps <- name_lookup(query = spvector[i], rank = "species", 
-                       return = "data", limit = 100) # information about the species
+    sps <- try(name_lookup(query = spvector[i], rank = "species", 
+                           return = "data", limit = 100), silent = TRUE) # information about the species
     
-    keys <- sps$key # all keys returned
-    counts <- vector() # object to save info on number of records per key
+    sps_class <- class(sps)
     
-    for (j in 1:length(keys)) { # testing if keys return records
-      counts[j] <- occ_count(taxonKey = keys[j], georeferenced = TRUE) 
-    }
-    
-    if (sum(counts) == 0) { # if no info, tell the species
-      cat("species", spvector[i], "has no goereferenced data\n")
+    # avoiding errors from GBIF (e.g., species name not in GBIF)
+    if(sps_class[1] == "try-error") {
+      occ_count[[i]] <- c(Species = spvector[i], keys = 0, counts = 0) # species not in GBIF
+      cat("species", spvector[i], "is not in the GBIF database\n")
       
-    }else { # if it has info, use the key with more records, which is the most useful
-      if (length(keys) == 1) { # if it is only one key
-        key <- keys # detecting species key 
-        occ_count[[i]] <- cbind(spvector[i], counts) # count how many records
-        
-      }else { # if its more than one key
-        keysco <- cbind(keys, counts)
-        keysco <- keysco[order(keysco[, 2]), ]
-        key <- keysco[dim(keysco)[1], 1] # detecting species key that return information
-        occ_count[[i]] <- keysco[dim(keysco)[1], ]# count how many records
+    }else {
+      keys <- sps$key # all keys returned
+      counts <- vector() # object to save info on number of records per key
+      
+      for (j in 1:length(keys)) { # testing if keys return records
+        counts[j] <- occ_count(taxonKey = keys[j], georeferenced = TRUE) 
       }
       
-      occ <- occ_search(taxonKey = key, return = "data", limit = 10000) # getting the data from GBIF
-      occ_g <- occ
-      occ_g <- occ_g[, c(1, 2, 4, 3, 5:dim(occ_g)[2])] # reordering longitude and latitude
-      
-      # keeping only unique georeferenced records. IF NO FILTERING IS NEEDED, PUT A # IN FRONT OF THE NEXT 3 LINES
-      occ_g <- occ_g[!is.na(occ_g$decimalLatitude) & !is.na(occ_g$decimalLongitude), ] # excluding no georeferences
-      occ_g <- occ_g[!duplicated(paste(occ_g$name, occ_g$decimalLatitude, # excluding duplicates
-                                       occ_g$decimalLongitude, sep = "_")), ]
-      
-      # writting file
-      file_name <- paste(gsub(" ", "_", spvector[i]), "csv", sep = ".") # csv file name per each species
-      write.csv(occ_g, paste(infolder, file_name, sep = "/"), row.names = FALSE) # writing inside each genus folder
-      
-      cat("    ", i, "of", length(spvector), "species\n") # counting species per genus 
+      if (sum(counts) == 0) { # if no info, tell the species
+        occ_count[[i]] <- c(Species = spvector[i], keys = "all", counts = 0) # species not in GBIF
+        cat("species", spvector[i], "has no goereferenced data\n")
+        
+      }else { # if it has info, use the key with more records, which is the most useful
+        if (length(keys) == 1) { # if it is only one key
+          key <- keys # detecting species key 
+          occ_count[[i]] <- cbind(spvector[i], counts) # count how many records
+          
+        }else { # if its more than one key
+          keysco <- cbind(keys, counts)
+          keysco <- keysco[order(keysco[, 2]), ]
+          key <- keysco[dim(keysco)[1], 1] # detecting species key that return information
+          occ_count[[i]] <- c(Species = spvector[i], keysco[dim(keysco)[1], ])# count how many records
+        }
+        
+        occ <- try(occ_search(taxonKey = key, return = "data", limit = 10000), silent = TRUE) # getting the data from GBIF
+        occ_class <- class(occ)
+        
+        # avoiding errors from GBIF
+        while (occ_class[1] == "try-error") {
+          occ <- try(occ_search(taxonKey = key, return = "data", limit = 10000), silent = TRUE) # getting the data from GBIF
+          occ_class <- class(occ)
+          
+          if(occ_class[1] != "try-error") {
+            break()
+          }
+        }
+        
+        # following steps
+        occ_g <- occ
+        occ_g <- occ_g[, c(1, 2, 4, 3, 5:dim(occ_g)[2])] # reordering longitude and latitude
+        
+        # keeping only unique georeferenced records. IF NO FILTERING IS NEEDED, PUT A # IN FRONT OF THE NEXT 3 LINES
+        occ_g <- occ_g[!is.na(occ_g$decimalLatitude) & !is.na(occ_g$decimalLongitude), ] # excluding no georeferences
+        occ_g <- occ_g[!duplicated(paste(occ_g$name, occ_g$decimalLatitude, # excluding duplicates
+                                         occ_g$decimalLongitude, sep = "_")), ]
+        
+        # writting file
+        file_name <- paste(gsub(" ", "_", spvector[i]), "csv", sep = ".") # csv file name per each species
+        write.csv(occ_g, paste(infolder, file_name, sep = "/"), row.names = FALSE) # writing inside each genus folder
+        
+        cat("    ", i, "of", length(spvector), "species\n") # counting species per genus 
+      }
     }
   }
   
-  occ_count[sapply(occ_count, is.null)] <- NULL # excluding countings with no data
   genus_data <- do.call(rbind, occ_count) # making the list of countings a table
-  genus_data <- data.frame(genus_data[, 1], as.numeric(genus_data[, 2])) # making countings numeric
-  names(genus_data) <- c("Species", "N_records") # naming columns
+  genus_data <- data.frame(genus_data[, 1], genus_data[, 2], as.numeric(genus_data[, 3])) # making countings numeric
+  names(genus_data) <- c("Species", "Key", "N_records") # naming columns
   
   # writing the table
   file_name1 <- paste(genus[h], "record_count.csv", sep = "_") # csv file name per each genus
