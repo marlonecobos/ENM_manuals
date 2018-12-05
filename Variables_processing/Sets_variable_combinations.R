752dfbac2e265b056b2b7f46170df1b1c63c8baf
Marlon E. Cobos <manubio13@gmail.com>
2018-12-05 00:46:35 -0600

    Var combination function and script improved

--- Variables_processing/Sets_variable_combinations.R
+++ Variables_processing/Sets_variable_combinations.R
@@ -4,17 +4,22 @@
 
 # data needed 
 ## Environmental raster layers, in this example we will use a set of ascii 
-## layers located in the folder (bio) in our working directory. Variables must 
-## be in ascii format.
+## layers located in the folder (bio) in our working directory. Variables can 
+## be ascii or tif files.
 
 ## Note: All variables must have the same projection, extent, and resolution.
 ## We suggest to work with Geographic projections WGS84, with no planar projection.
 
 source("https://raw.githubusercontent.com/marlonecobos/ENM_manuals/master/Variables_processing/kuenm_varcomb.R")
 
+setwd("YOUR/DIRECTORY")
+
 in_folder <- "bio" # name of folder with variables to be combined in distinct sets
 out_folder <- "Var_combinations" # name of folder that will contain the sets 
                                  # of combined variables (the function creates this)
 n_min <- 2 # minimum number of variables per combination
+in_format <- "ascii" # other option available is "GTiff"
+out_format <- "ascii" # other option available is "GTiff"
   
-kuenm_varcomb(vars = in_folder, out.vars = out_folder, min.number = n_min)
+kuenm_varcomb(var.dir = in_folder, out.dir = out_folder, min.number = n_min,
+              in.format = in_format, out.format = out_format)
--- Variables_processing/kuenm_varcomb.R
+++ Variables_processing/kuenm_varcomb.R
@@ -2,27 +2,49 @@
 #'
 #' @description kuenm_varcomb creates multiple sets of variables by grouping them in all their potential combinations.
 #'
-#' @param vars (character) the name of the folder containing variables (.asc format) that will be combined.
-#' @param out.vars (character) the name of the folder in which subfolders with distinct combinations of
-#' variables (.asc format) will be written.
+#' @param var.dir (character) the name of the folder containing variables that will be combined.
+#' @param out.dir (character) the name of the folder in which subfolders with distinct combinations of
+#' variables will be written.
 #' @param min.number (integer) the minimum number of variables per combination. This number must be > 1. 
 #' Default = 2.
+#' @param in.format (character) format of variables in \code{var.dir}. Options are "ascii" and "GTiff". 
+#' Default = "ascii".
+#' @param out.format (character) format of variables to be written in distinct sets inside \code{out.dir}. 
+#' Options are "ascii" and "GTiff". Default = "ascii".
 #'
-#' @return A list containing vectors of all the potential combinations of vars. In addition, a folder named out.vars 
-#' with subfolders in which the distinct combinations of variables produced were written.
+#' @return A list containing vectors of all the potential combinations of variables. In addition, a folder 
+#' named \code{out.dir} with subfolders in which distinct combinations of variables produced are written.
 #'
 #' @details Sest of variables are written in the working directory and not retained as RasterStacks to avoid
 #' problems related to RAM limitations.
 
 
-kuenm_varcomb <- function(vars, out.vars, min.number = 2) {
+kuenm_varcomb <- function(var.dir, out.dir, min.number = 2, in.format = "ascii", 
+                          out.format = "ascii") {
   
+  # Setting things up
   if (min.number < 2) {
     stop("min.number must be an integer > 1.")
   }
+  if (in.format == "ascii") {
+    patt <- ".asc$"
+  }
+  if (in.format == "GTiff") {
+    patt <- ".tif$"
+  }
+  if (out.format == "ascii") {
+    patt1 <- ".asc$"
+  }
+  if (out.format == "GTiff") {
+    patt1 <- ".tif$"
+  }
   
   # List variable names
-  variables <- list.files(path = paste(getwd(), vars, sep = "/"), pattern = ".asc")
+  variables <- list.files(path = var.dir, pattern = patt)
+  
+  if (length(variables) == 0) {
+    stop(paste("No variables with format", in.format, "were found in the directory", var.dir))
+  }
   
   if (min.number > length(variables)) {
     stop("min.number must be < the total number of variables.")
@@ -46,29 +68,41 @@ kuenm_varcomb <- function(vars, out.vars, min.number = 2) {
   var_combinations <- do.call(c, var_comb)
   
   # Preparing folders, variable combinations, and writing results
-  path <- paste(getwd(), out.vars, sep = "/")
-  dir.create(path)
+  dir.create(out.dir)
   
-  vars_all <- raster::stack(paste(getwd(), vars, variables, sep = "/"))
+  vars_all <- raster::stack(paste(var.dir, variables, sep = "/"))
   
-  sub_paths <- paste(path, paste("Set", 1:length(var_combinations), sep = "_"), sep = "/")
+  sub_paths <- paste(out.dir, paste("Set", 1:length(var_combinations), sep = "_"), sep = "/")
   
-  pb <- winProgressBar(title = "Progress bar", min = 0, max = length(sub_paths), width = 300) #progress bar
+  cat("\nA total of", length(sub_paths), "sets of variables resulted from combinations of", length(variables), "variables will be written.\n")
+  
+  if(.Platform$OS.type == "unix") {
+    pb <- txtProgressBar(min = 0, max = length(sub_paths), style = 3) #progress bar
+  } else {
+    pb <- winProgressBar(title = "Progress bar", min = 0, max = length(sub_paths), width = 300) #progress bar
+  }
   
   for (k in 1:length(sub_paths)) {
     Sys.sleep(0.1)
-    setWinProgressBar(pb, k, title = paste(round(k / length(sub_paths) * 100, 2),
-                                           paste("% of the process has finished")))
+    if(.Platform$OS.type == "unix") {
+      setTxtProgressBar(pb, i)
+    } else {
+      setWinProgressBar(pb, k, title = paste(round(k / length(sub_paths) * 100, 2),
+                                             paste("% of the process has finished")))
+    }
     
     dir.create(sub_paths[k])
-    vars_set <- vars_all[[gsub(".asc", "", var_combinations[[k]])]]
+    vars_set <- vars_all[[gsub(patt, "", var_combinations[[k]])]]
     
     for (l in 1:dim(vars_set)[3]) {
       raster::writeRaster(vars_set[[l]], filename = paste(sub_paths[k], var_combinations[[k]][l], sep = "/"), 
-                          format = "ascii")
+                          format = out.format)
     }
   }
-  suppressMessages(close(pb))
+  
+  if(.Platform$OS.type != "unix") {
+    suppressMessages(close(pb))
+  }
   
   return(var_combinations)
 }
