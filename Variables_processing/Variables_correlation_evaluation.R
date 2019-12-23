cef168edaedb552e4a988a587736d70f109ba70c
marlonecobos <manubio13@gmail.com>
2019-12-22 21:10:10 -0600

    Correlation easier

--- Variables_processing/Variables_correlation_evaluation.R
+++ Variables_processing/Variables_correlation_evaluation.R
@@ -2,78 +2,92 @@
 # Evaluation of variables correlation
 #####
 
-# data needed 
-## Environmental raster layers, in this example we will use a set of ascii 
-## layers located in the folder (bio) in our working directory. Our layers are 
-## 15 bioclimatic varaibles. These varaibles can be in other formats as well,
-## but make sure that you use the appropriate extension when reading them.
+# Description
+## The following script helps to measure correlation among distinct raster layers
+## to be used as predictors in ecological niche modeling. This process needs to
+## be performed in the area of model calibration.
 
 ## Note: All variables must have the same projection, extent, and resolution.
 ## We suggest to work with Geographic projections WGS84, with no planar projection.
 
+## The main processes are performed the package ellisenm from GitHub. To install
+## this package see instructions in https://github.com/marlonecobos/ellipsenm.
+
 # loading needed packages (packages will be automatically installed if required)
-pcakages <- c("raster", "rgdal")
-req_packages <- pcakages[!(pcakages %in% installed.packages()[, "Package"])]
-if (length(req_packages) > 0) {
-  install.packages(req_packages, dependencies = TRUE)
-}
-sapply(pcakages, require, character.only = TRUE)
+suppressWarnings({
+  if(!require(raster)){
+    install.packages("raster")
+    library(raster)
+  }
+}) 
+
+# assuming that you installed ellipsenm, load it, if not installed see 
+# https://github.com/marlonecobos/ellipsenm for instructions
+library(ellipsenm)
+
+
+#######################################################################################
+# Preparing directory and data
+##################
 
 # defining working directory
 setwd("D:/Marlon/Variables_processing/") # change this to your working directory
 
-# reading data
+# IF YOU HAVE THE DATA IN YOUR DIRECTORY AS DESCRIBED ABOVE, USE THIS
+# variables need to be saved in a subdirectory named "bio", variables must be in 
+# ascii format (.asc)
+
+## reading data
 varaibles_list <- list.files(path = "bio", pattern = ".asc", # vector of variables
                              full.names = TRUE)
 
 variables <- stack(varaibles_list) # stack of variables
 
-# getting data from the variables
-variables_values <- na.omit(values(variables))
 
-# sample of 10000 values if more pixels exist (optional)
-if (dim(variables_values)[1] > 10000) {
-  variables_values <- variables_values[sample(1:nrow(variables_values), 10000), ] 
-} 
+# IF YOU DON'T HAVE THE DATA, USE THIS
+## download data
+variables <- getData("worldclim", var = "bio", res = 10)[[-c(8, 9, 18, 19)]]
 
-# correlation matrix calculation
-correlation_matrix <- cor(variables_values)
+## crop the variables to an "invented" area for model calibration
+ext <- extent(-105, -85, 5, 25)
+variables <- crop(variables, ext)
 
-# saving correlation matrix
-write.csv(correlation_matrix, "variables_correlation_matrix.csv",
-          row.names = TRUE)
 
-# detecting correlated varaibles more easily
-correlation_matrix1 <- correlation_matrix # making other table with results
+#######################################################################################
+# Varriable correlation analysis
+##################
 
-max_cor <- 0.8 # maximum value of correlation allowed
+# to save matrix correlation results see the arguments "save" and "name"
 
-for (i in 1:dim(correlation_matrix1)[2]) { #correlated values will turn into 2 for easier detection
-  for (j in 1:dim(correlation_matrix1)[1]) {
-    correlation_matrix1[j, i] <- ifelse(correlation_matrix1[j, i] > max_cor | correlation_matrix1[j, i] < -max_cor, 
-                                        2, correlation_matrix1[j, i])
-  }
-}
+# correlation matrix
+cors <- variable_correlation(variables)
 
-# #checking the table
-View(correlation_matrix1) # selection should be done manually, 2 = correlated
+# checking the table
+View(cors) 
 
-# saving correlation matrix
-write.csv(correlation_matrix1, "variables_correlation_matrix2.csv",
-          row.names = TRUE)
+# analysis and ploting (all values above 0.75 will be magnified)
+cors1 <- variable_correlation(variables, correlation_limit = 0.75,
+                              corrplot = TRUE, magnify_to = 1.5)
+
+
+#######################################################################################
+# Selecting variables
+##################
 
 # selecting variables and writing them in a new directory
 names(variables) # names
 
-selected_variables <- variables[[c("bio_1", "bio_4", "bio_7", # select only non-correlated ones
-                                   "bio_12", "bio_15", "bio_17")]] 
+selected <- c("bio2", "bio4", "bio7", "bio13", "bio17") # change to the ones you select
 
-variable_names <- paste("Non_correlated_variables/", names(selected_variables), # names of variables
-                        ".asc", sep = "") 
+selected_variables <- variables[[selected]] # selecting
 
-dir.create("Non_correlated_variables") # create the folder for saving these variables
+## create the folder for selected variables
+dir.create("Non_correlated_variables") 
 
-for (i in 1:length(raster::unstack(selected_variables))) { # writing the selected variables
-  raster::writeRaster(selected_variables[[i]], filename = variable_names[i], 
-                      format = "ascii")
+## names of variables
+variable_names <- paste0("Non_correlated_variables/", selected, ".asc") 
+
+## writing selected variables
+for (i in 1:nlayers(selected_variables)) {
+  writeRaster(selected_variables[[i]], filename = variable_names[i], format = "ascii")
 }
